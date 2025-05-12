275c262d9f5da2290f98aded4c6624ab6b01d7cf
Doko Zero <17993323+dokozero@users.noreply.github.com>
2025-05-12 23:08:56 +0200

    Added vite-plugin-singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -8,5 +9,5 @@ export default defineConfig({
     outDir: '../../dist',
     emptyOutDir: true
   },
-  plugins: [react()]
+  plugins: [react(), viteSingleFile()]
 })
